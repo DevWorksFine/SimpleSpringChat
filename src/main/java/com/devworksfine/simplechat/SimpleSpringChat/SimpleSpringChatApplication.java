94c6e8297d490154a8576242d537de05fc35c92f
Vasil Katrandzhiev <vasil.katrandzhiev@scalefocus.com>
2019-02-26 09:19:07 +0200

    Initialized Spring project with websocket dependency

---
+++ src/main/java/com/devworksfine/simplechat/SimpleSpringChat/SimpleSpringChatApplication.java
@@ -0,0 +1,13 @@
+package com.devworksfine.simplechat.SimpleSpringChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringChatApplication.class, args);
+	}
+
+}
